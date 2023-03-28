19def84216168ffcf6f34c6393ebf5047b5f474e
Nana-Afia <naowusuachiaw@yahoo.com>
2023-03-28 04:16:21 -0700

    task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,15 +1,12 @@
 #include "binary_trees.h"
 /**
  * binary_tree_is_root - know if a node is a root, that means that the node
- *  has child both at left and right
  * @node: node to study
  * Return: 1 if it is a root or 0 if it is not
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL)
+	if (node == NULL || node->parent != NULL)
 		return (0);
-	if (node->left != NULL && node->right != NULL)
-		return (1);
-	return (0);
+	return (1);
 }
